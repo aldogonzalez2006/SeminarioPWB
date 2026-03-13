33ef378970427863d2391641d4b81950fcf25b51
Ihebel Tenorio <ihebel@ciencias.unam.mx>
2026-03-13 12:25:13 -0600

    termino practica

--- product/src/main/java/com/product/api/repository/RepoCategory.java
+++ product/src/main/java/com/product/api/repository/RepoCategory.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 
 import com.product.api.entity.Category;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RepoCategory extends JpaRepository<Category, Integer> {
